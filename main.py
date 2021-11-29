ceb5d42c2bf85a8b505ca97a6c06c0ad4f1c59ac
LuciFearium <61132760+LuciFearium@users.noreply.github.com>
2021-11-29 08:21:10 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,34 @@
+'''
+
+Internal - Dictionary for all items
+External - Item based or number based
+IB - External - List for selected items
+External - Enemy HP/MR
+External - AP Ratio
+IB - Internal - Filter Boots
+Internal - MR calculation (Passives)
+IB - Internal - AP Calculation (Passives)
+Internal - Damage Calculation
+Output - Spells to Kill
+
+'''
+
+items = {
+  "Deathcap": {'ap':120, 'passive':35},
+  "Rocketbelt": {'ap':90,'MPen':6, 'passive':5},
+  "Rift": {'ap':80,'passive':8},
+
+}
+
+#people = {1: {'name': 'John', 'age': '27', 'sex': 'Male'},
+#          2: {'name': 'Marie', 'age': '22', 'sex': 'Female'}}
+
+#print(people[1]['name'])
+
+#myList = ["Deathcap","Ludens"]
+
+#if "Deathcap" in myList:
+#  print("Deathcap is in the list:", "Deathcap" in myList)
+
+
+#print(len(myList))
